eb705c7fc9c97ffaa7ba6fca3c3fbb9fc7d81ee8
Lisa Song <lisasong@stanford.edu>
2019-05-14 13:12:21 -0700

    change training process for new PyTorch version

--- train.py
+++ train.py
@@ -72,17 +72,18 @@ def train(model, optimizer, loss_fn, dataloader, metrics, params):
                 # compute all metrics on this batch
                 summary_batch = {metric:metrics[metric](output_batch, labels_batch)
                                  for metric in metrics}
-                summary_batch['loss'] = loss.data[0]
+                summary_batch['loss'] = loss.data # summary_batch['loss'] = loss.data[0]
                 summ.append(summary_batch)
 
             # update the average loss
-            loss_avg.update(loss.data[0])
+            loss_avg.update(loss.data) # loss_avg.update(loss.data[0])
 
             t.set_postfix(loss='{:05.3f}'.format(loss_avg()))
             t.update()
 
     # compute mean of all metrics in summary
-    metrics_mean = {metric:np.mean([x[metric] for x in summ]) for metric in summ[0]}
+    metrics_mean = {metric:np.mean([x[metric].item() for x in summ]) for metric in summ[0]}
+    # metrics_mean = {metric:np.mean([x[metric] for x in summ]) for metric in summ[0]}
     metrics_string = " ; ".join("{}: {:05.3f}".format(k, v) for k, v in metrics_mean.items())
     logging.info("- Train metrics: " + metrics_string)
 
