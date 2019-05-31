49d5504894b0a8a77b8a90ca85afe591ed8690c9
bozhao-liu <bozhao91@stanford.edu>
2019-05-31 02:47:12 +0200

    Initialize Alexnet

---
+++ Alex/Data_loader.py
@@ -0,0 +1,103 @@
+import random
+import os
+
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+
+# borrowed from http://pytorch.org/tutorials/advanced/neural_style_tutorial.html
+# and http://pytorch.org/tutorials/beginner/data_loading_tutorial.html
+# define a training image loader that specifies transforms on images. See documentation for more details.
+def train_transformer_list(params):
+    train_transformer = transforms.Compose([
+        transforms.RandomHorizontalFlip(),  # randomly flip image horizontally
+        transforms.RandomVerticalFlip(),  # randomly flip image vertically
+        transforms.RandomRotation(180), # randomly rotate image by 180 degrees
+        transforms.Grayscale(num_output_channels=params.num_input_channels), # convert RGB image to greyscale (optional, 1 vs. 3 channels)
+        transforms.ToTensor()])  # transform it into a torch tensor
+    return train_transformer
+
+# loader for evaluation, no horizontal flip
+def eval_transformer_list(params):
+    eval_transformer = transforms.Compose([
+        transforms.Grayscale(num_output_channels=params.num_input_channels), 
+        # transforms.Resize([177, 128]),  # resize the image to 177x128 (remove if images are already 64x64)
+        transforms.ToTensor()])  # transform it into a torch tensor
+    return eval_transformer
+
+class FundusDataset(Dataset):
+    """
+    A standard PyTorch definition of Dataset which defines the functions __len__ and __getitem__.
+    """
+    def __init__(self, data_dir, transform):
+        """
+        Store the filenames of the jpgs to use. Specifies transforms to apply on images.
+
+        Args:
+            data_dir: (string) directory containing the dataset
+            transform: (torchvision.transforms) transformation to apply on image
+        """
+        self.filenames = os.listdir(data_dir)
+        self.filenames = [os.path.join(data_dir, f) for f in self.filenames if f.endswith('.png')]
+
+        # self.labels = [int(os.path.split(filename)[-1][0]) for filename in self.filenames]
+        self.labels = []
+        for filename in self.filenames:
+            imagename = os.path.split(filename)[-1]
+            ind = imagename.find('(')
+            label = imagename[(ind+1):(ind+3)]
+            finallabel = np.array(list(map(int, list(label))))
+            self.labels.append(finallabel)
+        self.transform = transform
+
+    def __len__(self):
+        # return size of dataset
+        return len(self.filenames)
+
+    def __getitem__(self, idx):
+        """
+        Fetch index idx image and labels from dataset. Perform transforms on image.
+
+        Args:
+            idx: (int) index in [0, 1, ..., size_of_dataset-1]
+
+        Returns:
+            image: (Tensor) transformed image
+            label: (int) corresponding label of image
+        """
+        image = Image.open(self.filenames[idx])  # PIL image
+        image = self.transform(image)
+        return image, self.labels[idx]
+
+
+def fetch_dataloader(types, data_dir, params):
+    """
+    Fetches the DataLoader object for each type in types from data_dir.
+
+    Args:
+        types: (list) has one or more of 'train', 'val', 'test' depending on which data is required
+        data_dir: (string) directory containing the dataset
+        params: (Params) hyperparameters
+
+    Returns:
+        data: (dict) contains the DataLoader object for each type in types
+    """
+    dataloaders = {}
+
+    for split in ['train', 'val', 'test']:
+        if split in types:
+            path = os.path.join(data_dir, "{}_data".format(split))
+
+            # use the train_transformer if training data, else use eval_transformer without random flip
+            if split == 'train':
+                dl = DataLoader(FundusDataset(path, train_transformer_list(params)), batch_size=params.batch_size, shuffle=True,
+                                        num_workers=params.num_workers,
+                                        pin_memory=params.cuda)
+            else:
+                dl = DataLoader(FundusDataset(path, eval_transformer_list(params)), batch_size=params.batch_size, shuffle=False,
+                                num_workers=params.num_workers,
+                                pin_memory=params.cuda)
+
+            dataloaders[split] = dl
+
+    return dataloaders
---
+++ Train_Alexnet.py
@@ -0,0 +1,281 @@
+import argparse
+import os
+import shutil
+import time
+import utils
+import logging
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torch.utils.data.distributed
+import Alex.alexnet as alexnet
+
+# set the seed
+torch.manual_seed(1)
+torch.cuda.manual_seed(1)
+import gc
+cwd = os.getcwd()
+import Alex.Data_loader as Data_loader
+
+parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+parser.add_argument('--data', metavar='DATA_PATH', default='./data/ResizedData',
+                    help='path to imagenet data (default: ./data/ResizedData)')
+parser.add_argument('--model_dir', default='experiments/greyscale', 
+                    help="Directory containing params.json")
+parser.add_argument('-j', '--workers', default=8, type=int, metavar='N',
+                    help='number of data loading workers (default: 8)')
+parser.add_argument('--epochs', default=500, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('-b', '--batch-size', default=256, type=int,
+                    metavar='N', help='mini-batch size (default: 256)')
+parser.add_argument('--learning_rate', default=0.01, type=float,
+                    metavar='LR', help='initial learning rate')
+parser.add_argument('--momentum', default=0.90, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--weight-decay', '--wd', default=5e-4, type=float,
+                    metavar='W', help='weight decay (default: 5e-4)')
+parser.add_argument('--print-freq', '-p', default=50, type=int,
+                    metavar='N', help='print frequency (default: 10)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+parser.add_argument('--world-size', default=1, type=int,
+                    help='number of distributed processes')
+parser.add_argument('--dist-backend', default='NCCL', type=str,
+                    help='distributed backend (gloo for cpu training, NCCL for GPU training')
+
+best_prec1 = 0
+
+
+def main():
+    global args, best_prec1
+    args = parser.parse_args()
+    
+    json_path = os.path.join(args.model_dir, 'params.json')
+    assert os.path.isfile(json_path), "No json configuration file found at {}".format(json_path)
+    params = utils.Params(json_path)
+
+    # use GPU if available
+    params.cuda = torch.cuda.is_available()
+
+    # Set the random seed for reproducible experiments
+    torch.manual_seed(230)
+    if params.cuda: torch.cuda.manual_seed(230)
+
+    # Set the logger
+    utils.set_logger(os.path.join(args.model_dir, 'train.log'))
+
+    # Create the input data pipeline
+    logging.info("Loading the datasets...")
+
+    args.distributed = args.world_size > 1
+
+    # create model
+    model = alexnet.alexnet(pretrained=True)
+    input_size = (178,128)
+
+    #load with GPU
+    model.features = torch.nn.DataParallel(model.features)
+    model.cuda()
+    
+    
+    # define loss function and optimizer
+    loss = nn.CrossEntropyLoss().cuda()
+
+    optimizer = torch.optim.Adam(model.parameters(), args.lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
+
+    cudnn.benchmark = True
+    
+    
+    #resume checkpoint
+    if args.resume:
+        if os.path.isfile(args.resume):
+            print("=> loading checkpoint '{}'".format(args.resume))
+            checkpoint = torch.load(args.resume)
+            args.start_epoch = checkpoint['epoch']
+            best_prec1 = checkpoint['best_prec1']
+            model.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(args.resume, checkpoint['epoch']))
+        else:
+            print("=> no checkpoint found at '{}'".format(args.resume))
+
+
+
+    # Data loading code
+    if not os.path.exists(args.data+'/train_data'):
+        print("==> Data directory"+args.data+"does not exits")
+        print("==> Please specify the correct data path by")
+        print("==>     --data <DATA_PATH>")
+        return
+    
+    dataloaders = Data_loader.fetch_dataloader(['train', 'val'], args.data_dir, params)
+
+    train_loader = dataloaders['train']
+
+    val_loader = dataloaders['train']
+
+    print(model)
+
+    validate(val_loader, model, loss)
+
+    for epoch in range(args.start_epoch, args.epochs):
+
+        # train for one epoch
+        train(train_loader, model, loss, optimizer, epoch)
+
+        # evaluate on validation set
+        prec1 = validate(val_loader, model, loss)
+
+        # remember best prec@1 and save checkpoint
+        is_best = prec1 > best_prec1
+        best_prec1 = max(prec1, best_prec1)
+        save_checkpoint({
+            'epoch': epoch + 1,
+            'state_dict': model.state_dict(),
+            'best_prec1': best_prec1,
+            'optimizer' : optimizer.state_dict(),
+        }, is_best)
+
+
+def train(train_loader, model, loss, optimizer, epoch):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    # switch to train mode
+    model.train()
+
+    end = time.time()
+    for i, (input, label) in enumerate(train_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        label = label.cuda()
+        input_var = torch.autograd.Variable(input)
+        label_var = torch.autograd.Variable(label)
+
+        # compute output
+        output = model(input_var)
+        cost = loss(output, label_var)
+
+        # measure accuracy and record cost
+        prec1, prec5 = accuracy(output.data, label, topk=(1, 5))
+        losses.update(cost.data[0], input.size(0))
+        top1.update(prec1[0], input.size(0))
+        top5.update(prec5[0], input.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        cost.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_freq == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                   epoch, i, len(train_loader), batch_time=batch_time,
+                   data_time=data_time, loss=losses, top1=top1, top5=top5))
+        gc.collect()
+
+
+def validate(val_loader, model, loss):
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    # switch to evaluate mode
+    model.eval()
+
+    end = time.time()
+    for i, (input, label) in enumerate(val_loader):
+        label = label.cuda()
+        input_var = torch.autograd.Variable(input, volatile=True)
+        label_var = torch.autograd.Variable(label, volatile=True)
+
+        # compute output
+        output = model(input_var)
+        cost = loss(output, label_var)
+
+        # measure accuracy and record cost
+        prec1, prec5 = accuracy(output.data, label, topk=(1, 5))
+        losses.update(cost.data[0], input.size(0))
+        top1.update(prec1[0], input.size(0))
+        top5.update(prec5[0], input.size(0))
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_freq == 0:
+            print('Test: [{0}/{1}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                   i, len(val_loader), batch_time=batch_time, loss=losses,
+                   top1=top1, top5=top5))
+
+    print(' * Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f}'
+          .format(top1=top1, top5=top5))
+
+    return top1.avg
+
+
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, 'model_best.pth.tar')
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def accuracy(output, label, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = label.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(label.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
+
+
+if __name__ == '__main__':
+    main()
